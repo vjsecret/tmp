283c623e6c9c09572231b2cc0eba4e811ad22d44
fred <fred@askey.com.tw>
2019-04-26 18:11:57 +0800

    add some info in practice of flask

--- prac_flask/tmpflask.py
+++ prac_flask/tmpflask.py
@@ -1,6 +1,10 @@
 #virtualenv -p /usr/bin/python3.5 VENV35
 #deactivate
 
+
+#https://blog.liang2.tw/posts/2015/09/flask-draw-member/#_2
+#https://medium.com/%E4%B8%80%E5%80%8B%E4%BA%BA%E7%9A%84%E6%96%87%E8%97%9D%E5%BE%A9%E8%88%88/python-flask-rest-api%E7%AD%86%E8%A8%98-869c3d2fee3
+
 # pip install -r requirements.txt
 # requirements.txt
 # Flask==0.10.1
